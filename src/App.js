a0aec1bfa219af025efe03e8a962378a0140068f
khris <khris56@hotmail.com>
2019-08-12 13:54:36 -0500

    list implemented

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list: [],
+      newName: '',
+      newLastName:''
+    }
+  }
+  nameChange(e){
+    this.setState({
+      newName: e.target.value
+    })
+  }
+  lastNameChange(e){
+    this.setState({
+      newLastName: e.target.value
+    })
+  }
+  enterHandle(e){
+    e.preventDefault();
+    this.setState( prevState => ({
+      list: [...prevState.list, {id: this.state.list.length+1, name: this.state.newName, lastname: this.state.newLastName}],
+      newName: '',
+      newLastName: ''
+    }))
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.enterHandle.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.newName} onChange={this.nameChange.bind(this)}/>
               </div>
-
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.newLastName} onChange={this.lastNameChange.bind(this)}/>
               </div>
-
               <div className="action">
                 <button type="submit" className="btn btn-primary">Agregar Invitado</button>
               </div>
             </form>
-
             <table className="table bordered-table table-striped">
               <thead>
                 <tr>
@@ -30,7 +53,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+              {this.state.list.map( (guest, index) =>
+                <tr key={index}>
+                  <td>{guest.name}</td>
+                  <td>{guest.lastname}</td>
+                </tr>
+              )}
               </tbody>
             </table>
           </div>
@@ -41,5 +69,3 @@ class App extends Component {
 }
 
 export default App
-
-
